6317e23e17ccc7dd7f410c2eabfda0f43c927d53
Vivekanand P V <vivekanandpv@outlook.com>
2021-01-07 12:42:38 +0530

    element query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { UserViewModel } from 'src/models/domain.models';
 import { UserEventArgs } from 'src/models/eventargs.model';
 
@@ -7,34 +7,15 @@ import { UserEventArgs } from 'src/models/eventargs.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  counter = 5;
+export class AppComponent implements AfterViewInit {
+  @ViewChild('message', { static: true })
+  messagePTag!: ElementRef<HTMLParagraphElement>;
 
-  style = {
-    color: 'blue',
-    'font-size': '24px',
-    'font-weight': '700',
-  };
-
-  onChangeClass() {
-    if (this.counter > 1) {
-      --this.counter;
-    } else {
-      this.counter = 5;
-    }
-  }
-
-  get css(): any {
-    //  variant 1: string
-    return `display-${this.counter}`;
-
-    //  variant 2: string[]
-    // return [`display-${this.counter}`, 'text-success', 'font-weight-bold'];
-
-    //  variant 3: configuration object
-    // return {
-    //   'text-success': true,
-    //   'display-3': false,
-    // };
+  ngAfterViewInit(): void {
+    this.messagePTag.nativeElement.innerHTML =
+      'Component sets the dom property';
+    this.messagePTag.nativeElement.classList.add('text-danger');
+    this.messagePTag.nativeElement.classList.add('display-2');
+    this.messagePTag.nativeElement.style.border = '1px solid blue';
   }
 }
